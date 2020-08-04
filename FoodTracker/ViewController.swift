b5efa5ccf2c4a01459ab14833206946e9df8364a
Abduqodiri Qurbonzoda <abduqodiri.qurbonzoda@jetbrains.com>
2020-08-04 20:11:00 +0300

    Initial Commit

---
+++ FoodTracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FoodTracker
+//
+//  Created by Abduqodiri Qurbonzoda on 04/08/2020.
+//  Copyright © 2020 Abduqodiri Qurbonzoda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
